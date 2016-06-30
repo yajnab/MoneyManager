cb6d4cf833de3eb8b040fc05fcfd23f22fd13013
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2016-06-30 17:21:10 +0530

    Partial function working. Database Handler is off

--- app/src/main/java/yajnab/com/moneymanager/MainActivity.java
+++ app/src/main/java/yajnab/com/moneymanager/MainActivity.java
@@ -58,32 +58,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        /*final AutoCompleteTextView txtPurpose = (AutoCompleteTextView) findViewById(R.id.autoTxtPurpose);
-        final EditText txtAmt = (EditText) findViewById(R.id.txtAmt);
-        final EditText txtDate = (EditText) findViewById(R.id.txtDate);
-        final Button addbtn = (Button) findViewById(R.id.addBtn);
-        final TextView chk = (TextView) findViewById(R.id.chk);
-
-        final dbHandler db = new dbHandler(this);
-
-        addbtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-                Date date = new Date();String dates=null;
-                if (!txtDate.getText().toString().equals("")) {dates=txtDate.getText().toString();} else {dates=dateFormat.format(date);}
-                String s = txtAmt.getText()+" " + dates+" "+ txtPurpose.getText();
-                chk.setText(dates);
-
-
-                db.addMoney(new Money(txtPurpose.getText().toString(),"mcredit", Float.parseFloat(String.valueOf(txtAmt.getText())), dates));
-                Log.d("MoneyManagerReader","Reading all MoneyInventory");
-                List<Money> moneyList = db.getRecords();
-                for(Money money : moneyList){
-                    String log =  money.getID()+"\t"+money.getPurpose()+"\t"+money.getType()+"\t"+money.getAmount()+"\t"+money.getDate();
-                    Log.d("MoneyManagerReader",log);
-                }
-
-            }         });*/
+        /**/
     }
 }
--- app/src/main/java/yajnab/com/moneymanager/mcredit.java
+++ app/src/main/java/yajnab/com/moneymanager/mcredit.java
@@ -3,9 +3,18 @@ package yajnab.com.moneymanager;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 
 /**
@@ -15,6 +24,34 @@ public class mcredit extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_mcredit, container, false);
+        View v= inflater.inflate(R.layout.fragment_mcredit, container, false);
+        final AutoCompleteTextView txtPurpose = (AutoCompleteTextView) v.findViewById(R.id.autoTxtPurpose);
+        final EditText txtAmt = (EditText) v.findViewById(R.id.txtAmt);
+        final EditText txtDate = (EditText) v.findViewById(R.id.txtDate);
+        final Button addbtn = (Button) v.findViewById(R.id.addBtn);
+        final TextView chk = (TextView) v.findViewById(R.id.chk);
+
+        //final dbHandler db = new dbHandler(this);
+
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                Date date = new Date();String dates=null;
+                if (!txtDate.getText().toString().equals("")) {dates=txtDate.getText().toString();} else {dates=dateFormat.format(date);}
+                String s = txtAmt.getText()+" " + dates+" "+ txtPurpose.getText();
+                chk.setText(dates);
+
+
+                /*db.addMoney(new Money(txtPurpose.getText().toString(),"mcredit", Float.parseFloat(String.valueOf(txtAmt.getText())), dates));
+                Log.d("MoneyManagerReader","Reading all MoneyInventory");
+                List<Money> moneyList = db.getRecords();
+                for(Money money : moneyList){
+                    String log =  money.getID()+"\t"+money.getPurpose()+"\t"+money.getType()+"\t"+money.getAmount()+"\t"+money.getDate();
+                    Log.d("MoneyManagerReader",log);
+                }*/
+
+            }         });
+        return v;
     }
 }
