b4df9bf111070e2ed7213b0cf67410ffbe56c147
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2016-07-03 18:04:16 +0530

    Added Code for Home Fragment

--- app/src/main/java/yajnab/com/moneymanager/mhome.java
+++ app/src/main/java/yajnab/com/moneymanager/mhome.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -23,7 +24,16 @@ public class mhome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mhome, container, false);
+        View v = inflater.inflate(R.layout.fragment_mhome, container, false);
+        final TextView txtHomeBal = (TextView) v.findViewById(R.id.txtHomeBal);
+        final TextView txtHomeTrans = (TextView) v.findViewById(R.id.txtHomeTrans);
+        final dbHandler db = new dbHandler(container.getContext());
+        String balance = String.valueOf(db.getLastRecord().getBalance());
+        String lTrans= db.getLastRecord().getPurpose() + "\t" + db.getLastRecord().getType() + "\t" + db.getLastRecord().getAmount() + "\t" + db.getLastRecord().getDate();
+        txtHomeBal.setText(balance);
+        txtHomeTrans.setText(lTrans);
+        return v;
+                
     }
 
 }
