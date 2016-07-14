0a9547fcc3b5acbe7fde58b3b8237955e9c969fa
Yajnavalkya Bandyopadhyay <yajnab@gmail.com>
2016-07-15 00:47:03 +0530

    Initial WebServer Coding for MoneyManager App

---
+++ insert.php
@@ -0,0 +1,36 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "petrol123";
+$dbname = "yajnab";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$purpose=mysqli_real_escape_string($conn, $_POST["prp"]);
+$type=mysqli_real_escape_string($conn, $_POST["type"]);
+$amt=mysqli_real_escape_string($conn, $_POST["amt"]);
+$date=mysqli_real_escape_string($conn, $_POST["date"]);
+$sql3="SELECT MAX(KEY_ID) AS micro FROM moneyinventory";
+$lid = $conn->query($sql3)->fetch_object()->micro;
+//$mlid=mysqli_real_escape_string($conn, $lid);
+$sql1 = "SELECT KEY_BALANCE FROM moneyinventory WHERE KEY_ID=".$lid;
+$baln = $conn->query($sql1)->fetch_object()->KEY_BALANCE;
+$balance = $baln - $amt;
+$sql = "INSERT INTO moneyinventory (KEY_PURPOSE, KEY_TYPE, KEY_AMT, KEY_DATE, KEY_BALANCE) VALUES ('$purpose', '$type', $amt, '$date', $balance)";
+$conn->query($sql);
+/*if($conn->query($sql) ===TRUE) {
+	$last_id = $conn->insert_id;
+	echo "Successfully added" .$last_id;
+	echo "Balance last is" .$baln;
+	echo "Lid is" .$lid;
+}
+else{
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}*/
+
+$conn->close();
+?>
---
+++ sform.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Hello Check!</title>
+	</head>
+	<body>
+		<form action="./insert.php" method="post" id="form1">
+			Purpose :<input type="text" name="prp"/><br/>
+			Type :<input type="text" name="type"/><br/>
+			Amount :<input type="text" name="amt"/><br/>
+			Date :<input type="text" name="date"/><br/>
+		</form>
+		<button type="submit" form="form1" value="Submit">Submit</button>
+	</body>
+</html>
